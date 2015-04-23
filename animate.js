9e4f5d109a9df9c8b5712ecd05eef2a3c28640d9
schung507 <schung507@gmail.com>
2015-04-23 16:55:22 -0700

    Added call to foursquare api

--- animate.js
+++ animate.js
@@ -1,24 +1,12 @@
-//function for start button
-// function mouseOver()
-// {
-// 	$("#start").hide();
-// 	$("#westwood").show();
-// 	$("#sawtelle").addClass("left");
-// 	$("#santamonica").addClass("right");
-// }
-
 $(document).ready(function() { 
     //checks difference between number of rows and ids. If none, guide is complete and code can be removed.
     //if a result is used in more that one question reduce the value or results by the number of reuses
     var rows = $('#qTable tr').length - 1; 
     var liids = $('#qTable li').length;
 
-
-
     $('#qTable li').addClass("circle");
 
-    $('#qTable li').on('click',function() 
-    {
+    $('#qTable li').on('click',function() {
     //style the selected answer
     $(this).addClass('selectedAnswer').siblings().removeClass('selectedAnswer');    
 
@@ -44,7 +32,22 @@ $(document).ready(function() {
 
     if(difHigh > 0) {
         window.scrollTo(0, difHigh)
-    }
+        }
     })
+
+        $.getJSON('https://api.foursquare.com/v2/venues/explore?near=westwood&query=italian+food&oauth_token=MEIANHQZROVVGEY4MBKNJMUYHKEPUXR2QL5HLNJP335ZDIJB&v=20150421',
+        
+           function(data) {
+            $.each(data.response.groups, function(key,value){
+                $.each(value.items, function(key2,value2){
+                    console.log(value2.venue);
+                    var content = '<p>' + value2.venue.name + '</p>';
+                    $(content).appendTo("#qTable");
+                }); 
+            });
+        });
+
+
+
 })
 

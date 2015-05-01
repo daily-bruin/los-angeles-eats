53d88080175d7e6d3b850370df042a16f811fd2f
Jasmine Kim <jasminekim95@yahoo.com>
2015-04-30 21:56:54 -0700

    added map

--- animate.js
+++ animate.js
@@ -11,6 +11,10 @@ var near;
 var query;
 var price;
 
+var lat;
+var lng;
+var map;
+
 $(document).ready(function() { 
     //checks difference between number of rows and ids. If none, guide is complete and code can be removed.
     //if a result is used in more that one question reduce the value or results by the number of reuses
@@ -20,78 +24,108 @@ $(document).ready(function() {
     $('#qTable li').addClass("circle");
 
     $('#qTable li').on('click',function() {
-    //style the selected answer
-    $(this).addClass('selectedAnswer').siblings().removeClass('selectedAnswer');    
+        //style the selected answer
+        $(this).addClass('selectedAnswer').siblings().removeClass('selectedAnswer');    
 
-    //hide all rows after the currently displayed row and remove selectedAnswer style
-    var rowCurrent = $(this).closest("tr").prevAll("tr").length + 2; 
-    var rowsAfter = ' tr:nth-child(n+' + rowCurrent + ')';
+        //hide all rows after the currently displayed row and remove selectedAnswer style
+        var rowCurrent = $(this).closest("tr").prevAll("tr").length + 2; 
+        var rowsAfter = ' tr:nth-child(n+' + rowCurrent + ')';
 
-    $('#qTable' + rowsAfter).hide().find('li').removeClass('selectedAnswer');
+        $('#qTable' + rowsAfter).hide().find('li').removeClass('selectedAnswer');
 
-    //show the next row that matches the question id
-    var italNum =  $(this).find('i').text();
-    if(italNum < 4){
-    var qNext = ' tr:nth-child(' + italNum + ')';
+        //show the next row that matches the question id
+        var italNum =  $(this).find('i').text();
+        if(italNum < 4){
+        var qNext = ' tr:nth-child(' + italNum + ')';
 
-    $('#qTable' + qNext).fadeIn(800);
+        $('#qTable' + qNext).fadeIn(800);
 
-    //scroll code to bring next question into view
-    var qNextPos = $('#qTable' + qNext).offset();
-    var qNextTop = qNextPos.top;
-    var qNextHigh = $('#qTable' + qNext).height();
-    var qNextBot = qNextHigh + qNextTop + 20; 
-    var scrHigh = $(window).innerHeight();
-    var difHigh = qNextBot - scrHigh;
+        //scroll code to bring next question into view
+        var qNextPos = $('#qTable' + qNext).offset();
+        var qNextTop = qNextPos.top;
+        var qNextHigh = $('#qTable' + qNext).height();
+        var qNextBot = qNextHigh + qNextTop + 20; 
+        var scrHigh = $(window).innerHeight();
+        var difHigh = qNextBot - scrHigh;
 
-    if(difHigh > 0) {
-        window.scrollTo(0, difHigh)
+        if(difHigh > 0) {
+            window.scrollTo(0, difHigh)
+            }
         }
-    }
 
-    var parent =  $(this).parent().attr('id');
+        var parent =  $(this).parent().attr('id');
 
-    if( parent == "near"){
-        near = $(this).attr('id');
-        console.log(near);
-    }
+        if( parent == "near"){
+            near = $(this).attr('id');
+            console.log(near);
+        }
 
-    if( parent == "query"){
+        if( parent == "query"){
+
+            
+            query = $(this).attr('id');
+        
+            if(  $(this).attr('id') == "random")
+            {
+                query = "";
+            }
+            console.log(query);
+        }
+
+        if( parent == "price")
+        {
+            price = $(this).attr('id');
+            console.log(price);
 
         
-        query = $(this).attr('id');
-    
-        if(  $(this).attr('id') == "random")
-        {
-            query = "";
+
+            $.getJSON('https://api.foursquare.com/v2/venues/explore?near='+near+',CA'+ '&radius=10'+'&query='+query+'+food&price='+price+'&oauth_token=MEIANHQZROVVGEY4MBKNJMUYHKEPUXR2QL5HLNJP335ZDIJB&v=20150421',
+                function(data) {
+                    var cap;
+                    var random;
+
+                    $.each(data.response.groups, function(key,value){
+                        cap = Object.keys(value.items).length; 
+                        console.log('cap: ' + cap);
+                        random = Math.floor(Math.random() * cap);
+                        //console.log(random);
+
+                        var venueObj = value.items[random].venue;
+                        lat = venueObj.location.lat;
+                        lng = venueObj.location.lng;
+
+                        console.log(lat);
+                        console.log(lng);
+
+
+                        var content = '<p>' + venueObj.name + '</p>' + '<p>' + venueObj.location.address + '</p><p>' + venueObj.location.city + ', ' + venueObj.location.state + ' ' + venueObj.location.postalCode +'</p>';
+                        $(content).appendTo("#qTable");
+                        showMap(lat, lng);
+                        var center = map.getCenter();
+                        google.maps.event.trigger(map, 'resize');
+                        google.maps.event.addDomListener(window, 'load');
+                        map.setCenter(center);
+                    });
+                });
         }
-        console.log(query);
-    
-    }
-
-    if( parent == "price"){
-        price = $(this).attr('id');
-
-        console.log(price);
 
 
-        $.getJSON('https://api.foursquare.com/v2/venues/explore?near='+near+'&query='+query+'&price='+price+'&oauth_token=MEIANHQZROVVGEY4MBKNJMUYHKEPUXR2QL5HLNJP335ZDIJB&v=20150421',
-    
-            function(data) {
-            $.each(data.response.groups, function(key,value){
-                $.each(value.items, function(key2,value2){
-                    console.log(value2.venue);
-                    var content = '<p>' + value2.venue.name + '</p>';
-                    $(content).appendTo("#qTable");
-                }); 
+        function showMap(latitude,longitude) {
+            var myLatlng = new google.maps.LatLng(latitude,longitude);
+            var mapOptions = {
+                zoom: 15,
+                center: myLatlng
+            };
+
+            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: 'Marker'
             });
-        });
-    }
-
-
-
-    
-
+        }
     })
+
 
 })

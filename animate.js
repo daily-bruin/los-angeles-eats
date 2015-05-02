ae1711384cbfcefcd565becab8838387f720e68b
Jasmine Kim <jasminekim95@yahoo.com>
2015-05-02 14:55:26 -0700

    added comments

--- animate.js
+++ animate.js
@@ -19,153 +19,155 @@ $(document).ready(function() {
 
     $('#qTable li').addClass("circle");
 
+
+    /*
+        super big function
+        ends with a map function
+     */
     $('#qTable li').on('click',function() {
-    //style the selected answer
-    $(this).addClass('selectedAnswer').siblings().removeClass('selectedAnswer');    
+    
+        //style the selected answer
+        $(this).addClass('selectedAnswer').siblings().removeClass('selectedAnswer');    
 
-    //hide all rows after the currently displayed row and remove selectedAnswer style
-    var rowCurrent = $(this).closest("tr").prevAll("tr").length + 2; 
-    var rowsAfter = ' tr:nth-child(n+' + rowCurrent + ')';
+        //hide all rows after the currently displayed row and remove selectedAnswer style
+        var rowCurrent = $(this).closest("tr").prevAll("tr").length + 2; 
+        var rowsAfter = ' tr:nth-child(n+' + rowCurrent + ')';
 
-    $('#qTable' + rowsAfter).hide().find('li').removeClass('selectedAnswer');
+        $('#qTable' + rowsAfter).hide().find('li').removeClass('selectedAnswer');
 
-    //show the next row that matches the question id
-    var italNum =  $(this).find('i').text();
-    if(italNum < 4){
-    var qNext = ' tr:nth-child(' + italNum + ')';
+        //show the next row that matches the question id
+        var italNum =  $(this).find('i').text();
 
-    $('#qTable' + qNext).fadeIn(800);
+        if(italNum < 4) {
+            var qNext = ' tr:nth-child(' + italNum + ')';
 
-    //scroll code to bring next question into view
-    var qNextPos = $('#qTable' + qNext).offset();
-    var qNextTop = qNextPos.top;
-    var qNextHigh = $('#qTable' + qNext).height();
-    var qNextBot = qNextHigh + qNextTop + 20; 
-    var scrHigh = $(window).innerHeight();
-    var difHigh = qNextBot - scrHigh;
+            $('#qTable' + qNext).fadeIn(800);
 
-    if(difHigh > 0) {
-        window.scrollTo(0, difHigh)
-        }
-    }
+            //scroll code to bring next question into view
+            var qNextPos = $('#qTable' + qNext).offset();
+            var qNextTop = qNextPos.top;
+            var qNextHigh = $('#qTable' + qNext).height();
+            var qNextBot = qNextHigh + qNextTop + 20; 
+            var scrHigh = $(window).innerHeight();
+            var difHigh = qNextBot - scrHigh;
 
-    var parent =  $(this).parent().attr('id');
+            if(difHigh > 0) {
+                window.scrollTo(0, difHigh)
+            }
 
-    if( parent == "near"){
-        near = $(this).attr('id');
-        console.log(near);
-    }
+        }   //end of if italiNum
 
-    if( parent == "query"){
+        var parent =  $(this).parent().attr('id');
 
+        if( parent == "near"){
+            near = $(this).attr('id');
+            console.log(near);
+        }   //end of if near
+
+        if( parent == "query"){
+
+            
+            query = $(this).attr('id');
         
-        query = $(this).attr('id');
-    
-        if(  $(this).attr('id') == "random")
-        {
-            query = "";
-        }
-        console.log(query);
-    
-    }
+            if(  $(this).attr('id') == "random")
+            {
+                query = "";
+            }
+            console.log(query);
+        
+        }   //end of if query
 
-    if( parent == "price"){
-        price = $(this).attr('id');
+        if( parent == "price"){
+            price = $(this).attr('id');
 
-        console.log(price);
+            console.log(price);
 
-       
+           
 
-        $.getJSON('https://api.foursquare.com/v2/venues/explore?near='+near+',CA&query='+query+'+food&price='+price+'&oauth_token=MEIANHQZROVVGEY4MBKNJMUYHKEPUXR2QL5HLNJP335ZDIJB&v=20150421',
-    
-            function(data) {
-                var cap;
-                var random;
-                var venueObj;
+            $.getJSON('https://api.foursquare.com/v2/venues/explore?near='+near+',CA&query='+query+'+food&price='+price+'&oauth_token=MEIANHQZROVVGEY4MBKNJMUYHKEPUXR2QL5HLNJP335ZDIJB&v=20150421',
+        
+                function(data) {
+                    var cap;
+                    var random;
+                    var venueObj;
 
-            $.each(data.response.groups, function(key,value){
-                
-                cap = Object.keys(value.items).length;
-                random = Math.floor(Math.random() *cap);
-                console.log(random);
-                venueObj = value.items[random].venue;
-               
-    
-                $('#qTable').remove();
-                $("<div></div>").attr('id','address').appendTo('body'); 
-                $("<div>Ew i wanna start over</div>").attr('id','ew').appendTo('body'); 
-                $("<div>Ugh give me another one</div>").attr('id','ugh').appendTo('body'); 
-                again(venueObj);
-
-                function again(venueObj){
-                var content = '<p>' + venueObj.name + '</p><p>' + venueObj.location.address + '</p><p>' + venueObj.location.city + ', ' + venueObj.location.state + ' ' + venueObj.location.postalCode + '</p>';
-                
-                
-                $(content).appendTo("#address");
-                console.log(venueObj.location.lat);
-                console.log(venueObj.location.lng);
-
-
-                google.maps.event.addDomListener(window, 'load', showMap(venueObj.location.lat, venueObj.location.lng, venueObj.name));
-                }
-
-                $('#ew').click( function() {
-                    window.location.reload(true);
-                });
-
-
-
-        function showMap(latitude,longitude) {
-            var myLatlng = new google.maps.LatLng(latitude,longitude);
-            var mapOptions = {
-                zoom: 15,
-                center: myLatlng
-            };
-
-            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map,
-                title: 'Marker'
-            });
-        }
-    })
-
-                $('#ugh').click( function() {
+                $.each(data.response.groups, function(key,value){
+                    cap = Object.keys(value.items).length;
                     random = Math.floor(Math.random() *cap);
                     console.log(random);
                     venueObj = value.items[random].venue;
-                    $('#address').empty();
+                   
+
+                    $('#qTable').remove();
+                    $("<div></div>").attr('id','address').appendTo('body'); 
+                    $("<div>Ew i wanna start over</div>").attr('id','ew').appendTo('body'); 
+                    $("<div>Ugh give me another one</div>").attr('id','ugh').appendTo('body'); 
                     again(venueObj);
-                });
-            
-            });
-        });
-    }
-            
 
-            var map;
-           
-            function showMap(latitude, longitude, name) {
-                var myLatlng = new google.maps.LatLng(latitude,longitude);
-                var mapOptions = {
-                    zoom: 12,
-                    center: myLatlng
-                 };
-                map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+                    function again(venueObj){
+                    var content = '<p>' + venueObj.name + '</p><p>' + venueObj.location.address + '</p><p>' + venueObj.location.city + ', ' + venueObj.location.state + ' ' + venueObj.location.postalCode + '</p>';
+                    
+                    
+                    $(content).appendTo("#address");
+                    console.log(venueObj.location.lat);
+                    console.log(venueObj.location.lng);
 
-                  var marker = new google.maps.Marker({
-                          position: myLatlng,
-                          map: map,
-                          title: name,
-                      });
-                } 
 
+                    google.maps.event.addDomListener(window, 'load', showMap(venueObj.location.lat, venueObj.location.lng, venueObj.name));
+                    }
+
+                    $('#ew').click( function() {
+                        window.location.reload(true);
+                    });
+
+
+
+                        function showMap(latitude,longitude) {
+                            var myLatlng = new google.maps.LatLng(latitude,longitude);
+                            var mapOptions = {
+                                zoom: 15,
+                                center: myLatlng
+                            };
+
+                            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+                            var marker = new google.maps.Marker({
+                                position: myLatlng,
+                                map: map,
+                                title: 'Marker'
+                            });
+                        }   //end of showMap
+                    })  //end of for each loop for response.groups
+
+                    $('#ugh').click( function() {
+                        random = Math.floor(Math.random() *cap);
+                        console.log(random);
+                        venueObj = value.items[random].venue;
+                        $('#address').empty();
+                        again(venueObj);
+                    });
                 
-    
+                }); //end of json function
+            } //end of if price
 
-    })
+        var map;
+       
+        function showMap(latitude, longitude, name) {
+            var myLatlng = new google.maps.LatLng(latitude,longitude);
+            var mapOptions = {
+                zoom: 12,
+                center: myLatlng
+             };
+            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+            var marker = new google.maps.Marker({
+                  position: myLatlng,
+                  map: map,
+                  title: name,
+            });
+        }   //end of showMap function
+
+    }); // end of super long function
 
 
-})
+});

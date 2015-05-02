ad7494d06f2e20cf2e88587e23c5781b10a67925
Jasmine Kim <jasminekim95@yahoo.com>
2015-05-02 14:35:13 -0700

    some last changes

--- animate.js
+++ animate.js
@@ -118,7 +118,7 @@ $(document).ready(function() {
             };
 
             map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-            
+
             var marker = new google.maps.Marker({
                 position: myLatlng,
                 map: map,
@@ -126,6 +126,4 @@ $(document).ready(function() {
             });
         }
     })
-
-
 })

246819f1f526a2f88599060d20e7fe0a64c76bd4
Jasmine Kim <jasminekim95@yahoo.com>
2015-05-18 15:33:02 -0700

    push to master

--- animate.js
+++ animate.js
@@ -10,6 +10,9 @@
 var near;
 var query;
 var price;
+var label1;
+var label2;
+var label3;
 
 $(document).ready(function() { 
     //checks difference between number of rows and ids. If none, guide is complete and code can be removed.
@@ -62,26 +65,30 @@ $(document).ready(function() {
         if( parent == "near"){
             near = $(this).attr('id');
             console.log(near);
-            var label = $(this).text();
-            label = label.slice(0, -1);
-            $('<div>'+label+'</div>').attr('class','circle').appendTo('#sidebar');
+            label1 = $(this).text();
+            label1 = label1.slice(0, -1);
+            console.log(label1)
+            
         }   //end of if near
 
         if( parent == "query"){
             query = $(this).attr('id');
             console.log(query);
-            var label = $(this).text();
-            label = label.slice(0, -1);
-            $('<div>'+label+'</div>').attr('class','circle').appendTo('#sidebar');
+            label2 = $(this).text();
+            label2 = label2.slice(0, -1);
+            console.log(label2);
         }   //end of if query
 
         if( parent == "price"){
             price = $(this).attr('id');
 
             console.log(price);
-            var label = $(this).text();
-            label = label.slice(0, -1);
-            $('<div>'+label+'</div>').attr('class','circle').appendTo('#sidebar');
+            label3 = $(this).text();
+            label3 = label3.slice(0, -1);
+            console.log(label3);
+
+          
+            
 
             $.getJSON('https://api.foursquare.com/v2/venues/explore?ll='+near+'&query='+query+'&price='+price+'&oauth_token=MEIANHQZROVVGEY4MBKNJMUYHKEPUXR2QL5HLNJP335ZDIJB&v=20150421',
         
@@ -92,10 +99,18 @@ $(document).ready(function() {
 
                 $.each(data.response.groups, function(key,value){
                     cap = Object.keys(value.items).length;
-
+                    console.log(cap);
+                    $("#pusheen").hide();
                     if (cap == 0){
-                         $('#qTable').remove();
-                        $("<div>Too demanding. Nonexistent.</div>").attr('id','ew').appendTo('body'); 
+                        $('#tablediv').remove();
+                        $("#map-canvas").hide();
+                        $("<div>Too demanding. Nonexistent.</div>").attr('id','refresh2').appendTo('#error');
+                        $("#pusheen").show();
+                        $("<img src='http://i.imgur.com/6NvNV.gif'></img>").appendTo('#pusheen');
+                        
+                        $('#refresh2').click( function() {
+                            window.location.reload(true);
+                        });
                     }
 
                     else {
@@ -105,11 +120,17 @@ $(document).ready(function() {
                    
                    
 
-                    $('#qTable').remove();
-                    $("<div></div>").attr('id','address').appendTo('body'); 
-                    $("<div>Start over</div>").attr('id','ew').appendTo('body'); 
-                    $("<div>Next choice</div>").attr('id','ugh').appendTo('body'); 
+                    $('#tablediv').remove();
+                    $('#sidebar').show();
+                    $('<li>'+label1+'</li>').appendTo('#sidebar ul');
+                    $('<li>'+label2+'</li>').appendTo('#sidebar ul');
+                    $('<li>'+label3+'</li>').appendTo('#sidebar ul');
+                    $("<div></div>").attr('id','address').appendTo('#restaurant');
+                    $('#refresh').css( "display", "inline-block");
+                    $('#next').css( "display", "inline-block");
                     again(venueObj);
+                    
+                    $('#restaurant').show();
 
                     function again(venueObj){
                         console.log(venueObj);
@@ -152,26 +173,27 @@ $(document).ready(function() {
 
 
                         google.maps.event.addDomListener(window, 'load', showMap(venueObj.location.lat, venueObj.location.lng, venueObj.name));
+                        $('#map').show();
                     }
 
 
-                        function showMap(latitude,longitude) {
-                            var myLatlng = new google.maps.LatLng(latitude,longitude);
-                            var mapOptions = {
-                                zoom: 15,
-                                center: myLatlng
-                            };
+                        // function showMap(latitude,longitude) {
+                        //     var myLatlng = new google.maps.LatLng(latitude,longitude);
+                        //     var mapOptions = {
+                        //         zoom: 15,
+                        //         center: myLatlng
+                        //     };
 
-                            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+                        //     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-                            var marker = new google.maps.Marker({
-                                position: myLatlng,
-                                map: map,
-                                title: 'Marker'
-                            });
-                        }   //end of showMap
+                        //     var marker = new google.maps.Marker({
+                        //         position: myLatlng,
+                        //         map: map,
+                        //         title: 'Marker'
+                        //     });
+                        // }   //end of showMap
 
-                        $('#ugh').click( function() {
+                        $('#next').click( function() {
                             random = Math.floor(Math.random() *cap);
                             console.log(random);
                             venueObj = value.items[random].venue;
@@ -182,7 +204,7 @@ $(document).ready(function() {
 
                     })  //end of for each loop for response.groups
 
-                    $('#ew').click( function() {
+                    $('#refresh').click( function() {
                         window.location.reload(true);
                     });
 
@@ -191,11 +213,11 @@ $(document).ready(function() {
             } //end of if price
 
         var map;
-       
+        
         function showMap(latitude, longitude, name) {
             var myLatlng = new google.maps.LatLng(latitude,longitude);
             var mapOptions = {
-                zoom: 12,
+                zoom: 15,
                 center: myLatlng
              };
             map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -207,7 +229,8 @@ $(document).ready(function() {
             });
         }   //end of showMap function
 
+
     }); // end of super long function
 
 
-});
+}); //load document
